b5180e759f2b71862959a2b46d4247075c436756
Estudiantes <Estudiantes@Labing70203>
2018-02-26 16:20:49 -0500

    Commit parqueadero 7

--- Parqueadero/src/parqueadero/Principal.java
+++ Parqueadero/src/parqueadero/Principal.java
@@ -1,10 +1,19 @@
 
 package parqueadero;
 
+import Vehiculos.Moto;
+
 public class Principal {
     
     public static void main (String [] args){
         
+        FichaVehiculo miFicha;
+        miFicha = new FichaVehiculo();
+        miFicha.setVisible(true);
         
+        Moto miMoto=new Moto();
+        
+        //Modificar
+        miFicha.miMotoEnFichaVehiculo = miMoto;
     }   
 }
